b07c4e9beda2a934fa36bc4331038a1988ce7134
Jimmy Hoang <jimmy_hoang@genpt.com>
2023-10-14 01:04:19 -0400

    Redid the prokect

--- ConsumerCat/App.js
+++ ConsumerCat/App.js
@@ -1,20 +1,20 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from 'react-native-screens/native-stack';
-
-import AuthNavigator from './navigation/AuthNavigator';
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    
   return (
-    
-    <NavigationContainer>
-        <AuthNavigator/>
-    </NavigationContainer>
-            
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
