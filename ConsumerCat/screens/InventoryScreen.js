0b2198355076d281db4f6ab14e0b1ef6fb404033
princessempel <princess.empel@gmail.com>
2023-10-16 14:17:20 -0400

    style changes and add user email as placeholder for display name

--- ConsumerCat/screens/InventoryScreen.js
+++ ConsumerCat/screens/InventoryScreen.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Alert, View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { styles } from '../Styles';
 import Item from '../components/Item';
+import { auth, db } from '../firebaseConfig';
 
 const InventoryScreen = () => {
     return (
         <View style={inv_styles.container}>
             <Text style={inv_styles.hello_text}>Hello!</Text>
+            <Text style={inv_styles.display_name}>{auth.currentUser.email}</Text>
             <Text style={styles.header}>Your Inventory</Text>
             <View style={styles.horizontal_line} />
             <Item text="banana" quantity={1}/>
@@ -31,7 +33,12 @@ const inv_styles = StyleSheet.create({
         fontSize: 22,
         fontWeight: "bold",
         marginHorizontal: 5
-        
+    },
+
+    display_name: {
+        fontSize: 14,
+        marginHorizontal: 5,
+        marginBottom: 30,
     }
 });
 
