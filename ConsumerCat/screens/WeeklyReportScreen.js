95fd460e505443c334455d54dc3e050d59cd4745
Andy Nguyen <andyng2002@gmail.com>
2023-10-18 19:34:20 -0400

    add weekly report image

--- ConsumerCat/screens/WeeklyReportScreen.js
+++ ConsumerCat/screens/WeeklyReportScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { Alert, View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native';
 import { styles } from '../Styles';
 
 const WeeklyReportScreen = () => {
@@ -7,6 +7,9 @@ const WeeklyReportScreen = () => {
         <View style={wkrp_styles.container}>
             <Text style={styles.header}>Weekly Report</Text>
             <View style={styles.horizontal_line} />
+            <View style={wkrp_styles.viewImage}>
+                <Image source={require('../assets/weekly.png')} style={wkrp_styles.image}/>
+            </View>
         </View>
     );
 };
@@ -20,6 +23,14 @@ const wkrp_styles = StyleSheet.create({
         backgroundColor: '#E3FDE0',
         padding: 20,
     },
+    viewImage: {
+        maxWidth: '100%',
+        maxHeight: '100%'
+    },
+    image: {
+        maxWidth: '50%',
+        maxHeight: '50%'
+    }
 });
 
 export default WeeklyReportScreen;
